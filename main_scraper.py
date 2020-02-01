a48b0bc029e4aea37375b58de2d2d9b0ef0f4a11
VortexIndustry <33431383+VortexIndustry@users.noreply.github.com>
2020-02-01 16:43:23 +0000

    Update to fix known bugs

--- main_scraper.py
+++ main_scraper.py
@@ -3,128 +3,78 @@ import re
 def main():
     url = 'https://intranet.exeter.ac.uk/emps/studentinfo/subjects/computerscience/programmes/2019new/?programmeId=2943'
     r = requests.get(url)
-    counter = 0
-    stage = 0
-    new_val = []
-    for i in range(10):
-        new_val.append([])
-    f_one = open("Stage1.txt", "w+")
-    f_two = open("Stage2.txt", "w+")
-    f_three = open("Stage3.txt", "w+")
-    f_four = open("Stage4.txt", "w+")
-    for line in enumerate(r):
-        if counter != 0 or '''class="stage"''' in str(line):
-            if stage == 0:
-                f_one.write(str(line))
-            elif stage == 1:
-                f_two.write(str(line))
-            elif stage == 2:
-                f_three.write(str(line))
-            elif stage == 3:
-                f_four.write(str(line))
-            if '</div>' in str(line) and counter != 0:
-                counter = 1
-                stage += 1
-                new_val[stage].append(str(line))
-            else:
-                counter = 1
-                new_val[stage].append(str(line))
-def cleanup():
+    new_contents = str(r.text)
+    new_array = []
+    #print(str(r.text))
+    for x in range(70):
+        x = re.search(r'''(<tr class="orGroup"><td class="first">)((<a href="../../modules/2019/.......">.......</a>)|[.]*)[A-Z|a-z| ]*(</td><td>)([|A-Z|a-z| ]*[|:]*[|-]*[0-9]*)*''', new_contents)
+        if(x is not None):
+            new_contents = re.sub(r'''(<tr class="orGroup"><td class="first">)((<a href="../../modules/2019/.......">.......</a>)|[.]*)[A-Z|a-z| ]*(</td><td>)([|A-Z|a-z| ]*[|:]*[|-]*[0-9]*)*''', "",new_contents,1)
+            new_array.append(x.group())
+        else:
+            x = re.search(r'''(<tr class=""><td class="first">)((<a href="../../modules/2019/.......">.......</a>)|[.]*)[A-Z|a-z| ]*(</td><td>)([|A-Z|a-z| ]*[|:]*[|-]*[0-9]*)*''', new_contents)
+            new_array.append(x.group())
+            new_contents = re.sub(r'''(<tr class=""><td class="first">)((<a href="../../modules/2019/.......">.......</a>)|[.]*)[A-Z|a-z| ]*(</td><td>)([|A-Z|a-z| ]*[|:]*[|-]*[0-9]*)*''', "",new_contents,1)
+            if x is None:
+                break
+    cleanup(new_array)
+def cleanup(new_array):
     ext = []
     names = []
-    for i in range(4):
-        file_o = open("Stage" + str(i+1) + ".txt", "r")
-        contents = file_o.read()
-        x = re.split('''../../modules/2019/*''', contents)
-        extensions = []
-        chuck = []
-        for index, dull in enumerate(x):
-            extensions_l = []
-            for char in x[index]:
-                if char == '''"''':
-                    break
-                extensions_l.append(char)
-            new_ext = ''.join(extensions_l)
-            #new_ext = re.sub("[0-9][0-9][0-9]", '', new_ext)
-            new_ext = new_ext.replace("(", "").replace(")", "")
-            new_ext = re.sub(", b'", '', new_ext)
-            new_ext = new_ext.replace("'", "")
-            new_ext = re.sub("^[0-9][0-9][0-9]", "", new_ext)
-            new_ext = new_ext.replace("/", "")
-            new_ext = new_ext[:7]
-            extensions.append(new_ext)
-            y = re.split('''">*''',x[index])
-            #print("==========" + str(y[1]))
-            z = re.split('''</a></td>''', str(y[1]))
-            for index, item in enumerate(z):
-                if "stage" in item or item == []:
-                    del z[index]
-                    continue
-                if "td" in item:
-                    new_indexl = 0
-                    new_indexr = 0
-                    for indexs, char in enumerate(item):
-                        if char == ">":
-                            new_indexl = indexs + 1
-                            break
-                    for indexs, char in enumerate(item):
-                        if char == "<" and indexs > new_indexl:
-                            new_indexr = indexs
-                            break
-                    new_str = item[new_indexl:new_indexr]
-                    new_str = re.sub("[0-9][0-9][0-9]", '', new_str)
-                    new_str = new_str.replace("(", "").replace(")", "")
-                    new_str = re.sub("', b'", '', new_str)
-                    if "<" not in new_str:
-                        chuck.append(new_str)
-        del extensions[0]
-        ext.append(extensions)
-        names.append(chuck)
+    new_vals = []
+    for x in new_array:
+        new_vals = x.split(">")
+        del new_vals[0]
+        del new_vals[0]
+        names.append(new_vals[len(new_vals)-1])
+        if "<" in new_vals[0][0]:
+            del new_vals[0]
+        strX = new_vals[0]
+        strX = strX.split("<")
+        print(new_vals)
+        ext.append(strX[0])
+        print(ext[len(ext)-1])
+    print("======================================")
+    print(names)
+    print(ext)
+    print("======================================")
     next_stage(ext, names)
 #############################################################
 
 def next_stage(ext, names):
-    dependencies = []
     deps = []
-    for index in range(4):
-        dependencies.append([])
-        deps.append([])
-        for index_l, naas in enumerate(names[index]):
-            print(str(names[index][index_l]) + " : " + str(ext[index][index_l]))
-    url = 'https://intranet.exeter.ac.uk/emps/studentinfo/subjects/computerscience/modules/2019/index.php/?moduleCode='
-    for index in range(4):
-        for module_code in range(len(ext[index])):
-            r = requests.get(url+str(ext[index][module_code]))
-            #print(ext[index][module_code])
-            new_vals = re.search(r"PRE-REQUISITE.MODULES</th>.(<td>)*(([A-Z][A-Z][A-Z][0-9|A-Z][0-9][0-9][0-9])(,.)*)+</td>+", r.text)
-            if new_vals is not None:
-                new_vals = re.findall(r"[A-Z][A-Z][A-Z][0-9|A-Z][0-9][0-9][0-9]", str(new_vals))
-                deps[index].append(new_vals)
-                #print(new_vals)
-            else:
-                deps[index].append("None")
-                r.close()
+    for index_l in range(len(names)):
+        print(str(names[index_l]) + " : " + str(ext[index_l]))
 
+    url = 'https://intranet.exeter.ac.uk/emps/studentinfo/subjects/computerscience/modules/2019/index.php/?moduleCode='
+    for module_code in range(len(ext)):
+        r = requests.get(url+str(ext[module_code]))
+        print(ext[module_code])
+        new_vals = re.search(r"PRE-REQUISITE.MODULES</th>.(<td>)*(([A-Z][A-Z][A-Z][0-9|A-Z][0-9][0-9][0-9])(,.)*)+</td>+", r.text)
+        if new_vals is not None:
+            new_vals = re.findall(r"[A-Z][A-Z][A-Z][0-9|A-Z][0-9][0-9][0-9]", str(new_vals))
+            deps.append(new_vals)
+        else:
+            deps.append("None")
+            r.close()
     print(list(deps))
-    print(str(len(deps[0]) + len(deps[1]) + len(deps[2]) + len(deps[3])))
+    print(len(deps))
     file_w = open("toDraw.txt", "w+")
-    for indexs in range(4):
-        for count in range(len(ext[indexs])):
-            try:
-                file_w.write(str(names[indexs][count])+":"+str(ext[indexs][count]) + \
-                ":"+str(deps[indexs][count][0]))
-                if deps[indexs][count] != "None":
-                    try:
-                        file_w.write(":" + str(deps[indexs][count][1])+"@")
-                    except:
-                        file_w.write("@")
-                else:
-                    file_w.write("one@")
-            except:
-                print("COUNTER" + str(count * index))
+    for count in range(len(ext)):
+        try:
+            file_w.write(str(names[count])+")"+str(ext[count]) + \
+            ")"+str(deps[count][0]))
+            if deps[count] != "None":
+                try:
+                    file_w.write(")" + str(deps[count][1])+"@")
+                except:
+                    file_w.write("@")
+            else:
+                file_w.write("one@")
+        except:
+            print("COUNTER" + str(count))
 
 ####################################################
 
 if __name__ == "__main__":
     main()
-    cleanup()
